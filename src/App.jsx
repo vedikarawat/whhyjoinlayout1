705d63d496c0a790e88882390678c243ace03006
Vedika Rawat <v.raw08@gmail.com>
2025-07-23 11:36:34 +0530

    file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Tagline from "./Tagline";
 import FlashcardDock from "./FlashcardDock";
+import Cards from "./new";
 function App(){
     return (
     <div className="min-h-screen bg-black flex flex-col items-center justify-center relative overflow-hidden px-3 sm:px-4 md:px-6 lg:px-8 py-4 sm:py-0">
-      {/* Background ambient glow */}
       <div className="absolute inset-0 bg-gradient-radial from-[#1e3a8a]/20 via-transparent to-transparent"></div>
 
       <div className="relative w-full flex flex-col items-center -mt-6 -sm:mt-6">
         <Tagline />
         <FlashcardDock />
+        {/* <Cards /> */}
       </div>
     </div>
   );
