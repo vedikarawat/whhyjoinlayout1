67c47c4d01bac01ece5c671e9ceea3505a1ddf50
Vedika Rawat <v.raw08@gmail.com>
2025-07-26 08:17:32 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Tagline from "./Tagline";
 // import FlashcardDock from "./FlashcardDock";
-import FlashcardDock from "./new";
+// import FlashcardDock from "./new";
 // import FlashcardDock from "./new2";
 // import FlashcardDock from "./new3";
+import CenteredImageCard from "./CenteredImageCard";
 function App(){
     return (
     <div className="min-h-screen bg-black flex flex-col items-center justify-center relative overflow-hidden px-3 sm:px-4 md:px-6 lg:px-8 py-4 sm:py-0">
@@ -11,7 +12,7 @@ function App(){
 
       <div className="relative w-full flex flex-col items-center -mt-6 -sm:mt-6">
         <Tagline />
-        <FlashcardDock />
+        <CenteredImageCard />
       </div>
     </div>
   );
