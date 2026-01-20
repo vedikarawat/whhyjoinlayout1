01ac4484fc16a9b72000bf7e7bd300d1d6a4c167
Vedika Rawat <v.raw08@gmail.com>
2026-01-20 19:41:30 +0530

    final changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import Docks from "./whyIEEE/whyIEEE";
 import WhyChooseUs from "./whyIEEE/WhyChoose";
-import FlashcardDock from "./FlashcardDock";
-// import Tagline from "./Tagline";
 function App(){
     return (
     <div className="min-h-screen bg-black flex flex-col items-center justify-center relative overflow-hidden px-3 sm:px-4 md:px-6 lg:px-8 py-4 sm:py-0">
       <div className="absolute inset-0 bg-gradient-radial from-[#1e3a8a]/20 via-transparent to-transparent"></div>
 
       <div className="relative w-full flex flex-col items-center -mt-6 -sm:mt-6">
-        {/* <Tagline /> */}
-        {/* <Docks /> */}
-        {/* <WhyChooseUs /> */}
-        <FlashcardDock/>
+        <WhyChooseUs />
         
       </div>
     </div>
